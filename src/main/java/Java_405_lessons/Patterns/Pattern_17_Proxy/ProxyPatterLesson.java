563e1eb381b135710450fe8e3548d46d992f6c17
Oleg Chumin <tschumin.oleg@gmail.com>
2023-01-19 14:05:40 +0300

    Add change

--- src/main/java/Java_405_lessons/Patterns/Pattern_17_Proxy/ProxyPatterLesson.java
+++ src/main/java/Java_405_lessons/Patterns/Pattern_17_Proxy/ProxyPatterLesson.java
@@ -1,14 +1,27 @@
 package Java_405_lessons.Patterns.Pattern_17_Proxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 public class ProxyPatterLesson {
     public static void main(String[] args) {
-        Reader reader = new MyReader();
-        reader.read("Hello ");
+//        Reader reader = new MyReader();
+//        reader.read("Hello ");
+//
+//        Reader reader1 = new ProxyReader();
+//        reader1.read("Hello ");
+
+        InvocationHandler invocationHandler = new ReaderInvocationHandler();
+        Object proxyInstance = Proxy.newProxyInstance(Main.class.getClassLoader(), new Class[] {Reader.class}, invocationHandler);
+        ((Reader) proxyInstance).read("Hello !");
+
     }
 }
 
 interface Reader {
     String read(String str);
+
 }
 
 class MyReader implements Reader {
@@ -16,5 +29,28 @@ class MyReader implements Reader {
     @Override
     public String read(String str) {
         return str + "!";
+    }
+}
+
+//Proxy -> do something before and after method
+class ProxyReader extends MyReader {
+    @Override
+    public String read(String str) {
+        System.out.println(str); // here could be logging
+        String read = super.read(str);
+        System.out.println(read);
+        return null;
+    }
+}
+
+//II
+class ReaderInvocationHandler implements InvocationHandler {
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println(args[0]);
+        Object result = method.invoke(new MyReader(), args);
+        System.out.println(result);
+        return result;
     }
 }
