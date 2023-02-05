cfb676eb62e73a556acd7f528519b182b002a02c
Oleg Chumin <tschumin.oleg@gmail.com>
2023-02-05 09:51:01 +0300

    Add change

--- src/main/java/Java_405_lessons/Patterns/Pattern_16_Prototype/Step_03/PrototypePatternLesson.java
+++ src/main/java/Java_405_lessons/Patterns/Pattern_16_Prototype/Step_03/PrototypePatternLesson.java
@@ -3,25 +3,52 @@ package Java_405_lessons.Patterns.Pattern_16_Prototype.Step_03;
 /** Deep and non-deep cloning*/
 public class PrototypePatternLesson {
     public static void main(String[] args) throws CloneNotSupportedException {
-        Person person = new Person("Leo", 25);
+        Person person = new Person("Leo", 25, new Address("street", 23));
         Person person2 = person.clone();
         System.out.println("person2 != person : " + (person2 != person));
         System.out.println("person2.name == person.name : " + (person2.name == person.name));
         System.out.println("person2.age == person2.age : " + (person2.age == person2.age));
-        System.out.println("---------------------------------------------");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("person2.address.street == person2.address.street : " +
+                (person2.address.street == person2.address.street));
+        System.out.println("person2.address.houseNumber == person2.address.houseNumber : " +
+                (person2.address.houseNumber == person2.address.houseNumber));
+        System.out.println("--------------------------------------------------------------");
+
+        person.name = "Ilia";
+        System.out.println("person.name = " + person.name);
+        System.out.println("person2.name = " + person2.name);
+        System.out.println("--------------------------------------------------------------");
+
+        person.address.street = "SomeNewStreet";
+        System.out.println("person.address.street = " + person.address.street); //non deep cloning
+        System.out.println("person2.address.street = " + person2.address.street); //surface cloning
+        System.out.println("--------------------------------------------------------------");
+    }
+}
+
+class Address {
+    String street;
+    int houseNumber;
+
+    public Address(String street, int houseNumber) {
+        this.street = street;
+        this.houseNumber = houseNumber;
     }
 }
 
 class Person implements Cloneable {
     String name;
     int age;
+    Address address;
 
-    public Person(String name, int age) {
+    public Person(String name, int age, Address address) {
         this.name = name;
         this.age = age;
+        this.address = address;
     }
 
-//    @Override
+    //    @Override
 //    protected Object clone() throws CloneNotSupportedException {
 //        return super.clone();
 //    }
