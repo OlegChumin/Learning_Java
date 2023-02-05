b2d69743c5aed915ac934b2784d18c39c73ba897
Oleg Chumin <tschumin.oleg@gmail.com>
2023-02-05 10:24:50 +0300

    Add change

--- src/main/java/Java_405_lessons/Patterns/Pattern_18_Singleton/SingletonPattern.java
+++ src/main/java/Java_405_lessons/Patterns/Pattern_18_Singleton/SingletonPattern.java
@@ -3,19 +3,24 @@ package Java_405_lessons.Patterns.Pattern_18_Singleton;
 // можно создать только один объект класса
 public class SingletonPattern {
     public static void main(String[] args) {
-        Singleton singleton = Singleton.getInstance();
-        Singleton singleton1 = Singleton.getInstance();
-        singleton1.i = 5;
-        System.out.println(singleton.i);
+        SingletonSample singleton = SingletonSample.getInstance();
+        SingletonSample singleton2 = SingletonSample.getInstance();
+        singleton2.i = 5;
+        System.out.println("singleton.i = " + singleton.i);
+        System.out.println("singleton2.i = " + singleton2.i);
+        System.out.println("singleton2 == singleton : " + (singleton2 == singleton));
+        System.out.println("singleton = " + singleton);
+        System.out.println("singleton2 = " + singleton2);
+
     }
 }
 
-class Singleton{
+class SingletonSample{
     int i = 0;
-    static Singleton singleton = new Singleton();
-    private Singleton() {}
+    static SingletonSample singleton = new SingletonSample();
+    private SingletonSample() {}
 
-    public static Singleton getInstance() {
+    public static SingletonSample getInstance() {
         return singleton;
     }
 
