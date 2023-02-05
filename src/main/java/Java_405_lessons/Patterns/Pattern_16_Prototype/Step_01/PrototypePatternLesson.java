198c65f5bea773cf0576ae7331c1f6afb4142963
Oleg Chumin <tschumin.oleg@gmail.com>
2023-02-05 09:38:51 +0300

    Add change

--- src/main/java/Java_405_lessons/Patterns/Pattern_16_Prototype/Step_01/PrototypePatternLesson.java
+++ src/main/java/Java_405_lessons/Patterns/Pattern_16_Prototype/Step_01/PrototypePatternLesson.java
@@ -1,13 +1,11 @@
 package Java_405_lessons.Patterns.Pattern_16_Prototype.Step_01;
 
-import jdk.internal.HotSpotIntrinsicCandidate;
-
 public class PrototypePatternLesson {
     public static void main(String[] args) {
         // taking some element from database and if the element required more times
         //so copy of available object is the core of prototyping
         Person person = new Person("Leo", 25);
-//        Object object = new Object();
+//        Object object = new Object(); -> Clonable
 //        @HotSpotIntrinsicCandidate
 //        protected native Object clone() throws CloneNotSupportedException;
     }
---
+++ src/main/java/Java_405_lessons/Patterns/Pattern_16_Prototype/Step_02/PrototypePatternLesson.java
@@ -0,0 +1,37 @@
+package Java_405_lessons.Patterns.Pattern_16_Prototype.Step_02;
+
+public class PrototypePatternLesson {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Person person = new Person("Leo", 25);
+        Person person2 = person.clone();
+        System.out.println("person2 != person : " + (person2 != person));
+        System.out.println("person2.name == person.name : " + (person2.name == person.name));
+        System.out.println("person2.age == person2.age : " + (person2.age == person2.age));
+        System.out.println("---------------------------------------------");
+    }
+}
+
+class Person implements Cloneable {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+//    @Override
+//    protected Object clone() throws CloneNotSupportedException {
+//        return super.clone();
+//    }
+
+    @Override
+    protected Person clone() throws CloneNotSupportedException {
+        return (Person) super.clone();
+    }
+
+
+    //    public Person copy(Person person) {
+//        return new Person(person.name, person.age); //copy all fields
+//    }
+}
