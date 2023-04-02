32d4ed5cdba2ebecdba1cfd6dfe4a1376d8bd674
Oleg <tschumin.oleg@gmail.com>
2023-04-02 22:37:24 +0300

    Add new files

--- src/main/java/HackerRank/exam/Solution.java
+++ src/main/java/HackerRank/exam/Solution.java
@@ -1,51 +1,36 @@
 package HackerRank.exam;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.Stack;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = reader.readLine().split(" ");
-        String dataType = input[0];
-        int n = Integer.parseInt(input[1]);
-        if (dataType.equals("Integer")) {
-            Integer[] arr = new Integer[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(reader.readLine());
-            }
-            System.out.println(Arithmetic.sum(arr));
-        } else if (dataType.equals("String")) {
-            String[] arr = new String[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = reader.readLine();
-            }
-            System.out.println(Arithmetic.sum(arr));
-        }
+        String s = reader.readLine();
+        String result = Parser.isBalanced(s);
+        System.out.println(result);
     }
 }
 
-class Arithmetic {
-    public static int sum(Integer[] arr) {
-        int sum = 0;
-        for (Integer num : arr) {
-            sum += num;
+class Parser {
+    static String isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return "false";
+                }
+                char last = stack.pop();
+                if ((c == ')' && last != '(') || (c == '}' && last != '{') || (c == ']' && last != '[')) {
+                    return "false";
+                }
+            }
         }
-        return sum;
+        return stack.isEmpty() ? "true" : "false";
     }
 
-    public static String sum(String[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (String s : arr) {
-            sb.append(s);
-        }
-        return sb.toString();
-    }
 }
