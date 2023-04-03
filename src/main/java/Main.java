c0b6e568d87bb7c2b36e0a253ded3c92d2cc4f13
Oleg <tschumin.oleg@gmail.com>
2023-04-03 09:23:35 +0300

    Add new files

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String buy = "bitcoin";
+
+//        System.out.println(buy.substring(x, x + 1) + buy.substring(y, y + 2));
+    }
+
+}
