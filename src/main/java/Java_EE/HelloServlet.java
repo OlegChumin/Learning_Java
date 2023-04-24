5b168d6a7eab46e8d55f6c2e25fb5baaad067c51
Oleg <tschumin.oleg@gmail.com>
2023-04-24 21:02:45 +0300

    Add new files

---
+++ src/main/java/Java_EE/HelloServlet.java
@@ -0,0 +1,23 @@
+package Java_EE;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body><h1>Hello, world!</h1></body></html>");
+    }
+}
