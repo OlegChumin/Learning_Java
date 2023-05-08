34160bb62d8bb26ead23351b27774380e2651367
Oleg <tschumin.oleg@gmail.com>
2023-05-08 15:15:27 +0300

    Add new files

--- src/main/java/HackerRank/PrepaireToInterview/task014/Solution2.java
+++ src/main/java/HackerRank/PrepaireToInterview/task014/Solution2.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 class Solution2 {
 
-    private static final String REGEX = "";
+    private static final String REGEX = "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.)){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
 
     public static void main(String[] args) {
         String IPtoCheck = null;
