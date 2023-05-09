39bd293d90e057e9f82fd8b02caf7058f3a9878a
Oleg <tschumin.oleg@gmail.com>
2023-05-09 12:40:03 +0300

    Add new files

--- src/main/java/SOLID/_3_liskov_substitution/no_lsp/Bird.java
+++ src/main/java/SOLID/_3_liskov_substitution/no_lsp/Bird.java
@@ -1,5 +1,13 @@
 package SOLID._3_liskov_substitution.no_lsp;
 
+/**
+ * L - Liskov Substitution Principle (LSP) - Принцип подстановки Лисков:
+ * Объекты базового класса должны быть заменяемы объектами производных классов,
+ * не влияя на корректность программы.
+ * Пример:
+ *
+ * */
+
 public class Bird {
     public void fly() { /*...*/ }
 }
