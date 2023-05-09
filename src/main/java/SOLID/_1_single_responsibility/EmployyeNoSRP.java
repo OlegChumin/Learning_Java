5388ae078c67621dc667853d0638ac65ca08a14b
Oleg <tschumin.oleg@gmail.com>
2023-05-09 12:32:58 +0300

    Add new files

---
+++ src/main/java/SOLID/_1_single_responsibility/EmployyeNoSRP.java
@@ -0,0 +1,8 @@
+package SOLID._1_single_responsibility;
+
+// Соблюдение SRP
+public class EmployyeNoSRP {
+    public void calculateSalary() { /*...*/ }
+}
+
+
---
+++ src/main/java/SOLID/_1_single_responsibility/ReportPrinter.java
@@ -0,0 +1,6 @@
+package SOLID._1_single_responsibility;
+
+// Соблюдение SRP
+public class ReportPrinter {
+    public void printReport() { /*...*/ }
+}
