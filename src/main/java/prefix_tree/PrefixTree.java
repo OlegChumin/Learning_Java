41fbb899ed2f115676eb8de8c82145e45a5b64b2
Oleg <tschumin.oleg@gmail.com>
2023-05-11 20:39:31 +0300

    Add new files

--- src/main/java/prefix_tree/PrefixTree.java
+++ src/main/java/prefix_tree/PrefixTree.java
@@ -1,5 +1,48 @@
 package prefix_tree;
 
-public class PrefixTree {
+import java.util.ArrayList;
+import java.util.List;
 
+public class PrefixTree {
+    public static void main(String[] args) {
+
+    }
+
+    static class TreeNode {
+        char value;
+        List<TreeNode> children;
+
+        public TreeNode(char value) {
+            this.value = value;
+        }
+
+        public void insert(String data) {
+            if (data.length() == 0) {
+                return;
+            }
+            if (children == null) {
+                children = new ArrayList<>();
+            }
+            char c = data.charAt(0);
+            TreeNode child = findNodeByChar(c);
+            if (child == null) {
+                child = new TreeNode(c);
+                children.add(child);
+            }
+        }
+
+        //поиск в массиве дочерних узлов искать узел по его символу
+        private TreeNode findNodeByChar(char c) {
+            if (children != null) {
+                for (TreeNode node : children) {
+                    if (node.value == c) {
+                        return node;
+                    }
+                }
+            }
+            return null;
+        }
+
+
+    }
 }
