f582efae508b167f5bff3e7939cbd569780a94aa
Oleg <tschumin.oleg@gmail.com>
2023-05-13 08:36:48 +0300

    Add new files

--- src/main/java/OOP/Test.java
+++ src/main/java/OOP/Test.java
@@ -1,5 +1,6 @@
 package OOP;
 
+import Java_Nail_Alishev.old.BaseJava.interfaces.Animal;
 import org.apache.logging.log4j.core.util.DummyNanoClock;
 
 import java.util.Locale;
@@ -7,6 +8,7 @@ import java.util.Scanner;
 
 public class Test {
     public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         String animalName = getStringFromConsole();
         setAnimal(animalName);
@@ -14,13 +16,23 @@ public class Test {
     }
 
     public static void setAnimal(String animalName) {
-        AnimalNew animal =  new AnimalNew();
+        AnimalNew animal = new AnimalNew();
         switch (animalName) {
-            case "Dog": {Dog dog = new Dog(); dog.sleep(); dog.canBark();}
+            case "Dog": {
+                Dog dog = new Dog();
+                dog.sleep();
+                dog.canBark();
+            }
             break;
-            case "Cat": {Cat cat = new Cat(); cat.sleep();}
+            case "Cat": {
+                Cat cat = new Cat();
+                cat.sleep();
+            }
             break;
-            case "Duck": {}
+            case "Duck": {
+                AnimalNew duck = new Duck();
+                break;
+            }
             default:
                 System.out.println("No such animal");
         }
