dd7ee7fcd6cd5a786f12483e07b7fc9cd9d144cc
OlegChumin <tschumin.oleg@gmail.com>
2023-05-27 07:08:09 +0300

    Add new file

---
+++ src/main/java/Benchmark/StreamsBenchmark.java
@@ -0,0 +1,17 @@
+package Benchmark;
+
+/**
+ * Stream<T>
+ * IntStream        Stream<Integer>
+ * LongStream       Stream<Long>
+ * DoubleStream     Stream<Double>
+ * */
+public class StreamsBenchmark {
+    public static void main(String[] args) {
+
+    }
+
+    public static long stream() {
+        return Stream.of(integerss)
+    }
+}
