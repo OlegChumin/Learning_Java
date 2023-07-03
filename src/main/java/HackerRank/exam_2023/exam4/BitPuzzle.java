848fff6b9d0b266b52fd434b18855c360d14023e
Oleg Chumin <tschumin.oleg@gmail.com>
2023-07-03 13:03:28 +0300

    CRUDE code

--- src/main/java/HackerRank/exam_2023/exam4/BitPuzzle.java
+++
@@ -1,9 +0,0 @@
-package HackerRank.exam_2023.exam4;
-
-public class BitPuzzle {
-    public static void main(String[] args) {
-        int mask = 0x000F;
-        int value = 0x2222;
-        System.out.println(value & mask);
-    }
-}
