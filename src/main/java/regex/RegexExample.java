66f815364054b8e2f7ee95e8f94106c5ddb2597e
Oleg Chumin <tschumin.oleg@gmail.com>
2023-08-02 08:40:05 +0300

    CRUDE code

--- src/main/java/regex/RegexExample.java
+++ src/main/java/regex/RegexExample.java
@@ -4,11 +4,15 @@ import java.util.regex.*;
 
 public class RegexExample {
     public static void main(String[] args) {
-        String text = "The quick brown fox jumps over the lazy dog";
+        String text = "The quick brown fox jumps over the lazy dog"; //строка с регулярным выражением
 
         // Компилируем регулярное выражение для поиска слова "quick"
-        Pattern pattern = Pattern.compile("\\bquick\\b");
-        Matcher matcher = pattern.matcher(text);
+        Pattern pattern = Pattern.compile("\\bquick\\b"); // Создает объект Pattern, представляющий
+        // скомпилированное регулярное выражение.
+
+        Matcher matcher = pattern.matcher(text); //Создает объект Matcher, который будет использоваться для поиска
+        // соответствий регулярному выражению в input. input может быть строкой или другим объектом, реализующим
+        // интерфейс CharSequence, например, String, StringBuffer, или StringBuilder.
 
         // Поиск соответствия регулярному выражению
         if (matcher.find()) {
