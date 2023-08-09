ce60e24d7044a8ef5b07f198f61d0c397d51e771
Oleg Chumin <tschumin.oleg@gmail.com>
2023-08-09 16:04:48 +0300

    CRUDE code

--- src/main/java/API/WeatherApp.java
+++ src/main/java/API/WeatherApp.java
@@ -23,12 +23,24 @@ package API;
  */
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Properties;
 
 public class WeatherApp {
     public static void main(String[] args) {
+        Properties properties = new Properties();
+        try (InputStream inputStream = WeatherApp.class.getClassLoader().getResourceAsStream("myconfig.properties")) {
+            properties.load(inputStream);
+            String apiKey = properties.getProperty("api.key");
+            // Используйте apiKey в вашем коде
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         String apiKey = " "; // Замените на свой ключ OpenWeatherMap API
         String city = "Moscow";
         String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
