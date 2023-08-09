407eb03e706b71fa98698be471f0e69fabe29add
Oleg Chumin <tschumin.oleg@gmail.com>
2023-08-09 16:23:15 +0300

    CRUDE code

--- src/main/java/API/WeatherApp.java
+++ src/main/java/API/WeatherApp.java
@@ -28,15 +28,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class WeatherApp {
     public static void main(String[] args) {
         String apiKey = " "; // Initialize with a default value
         Properties properties = new Properties();
-        try (InputStream inputStream = WeatherApp.class.getClassLoader().getResourceAsStream("src/main/resources/API/myconfig.properties")) {
-            properties.load(inputStream);
-            apiKey = properties.getProperty("api.key").toString();
+        try (InputStream inputStream = WeatherApp.class.getClassLoader().getResourceAsStream("API/myconfig.properties")) {
+            properties.load(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            apiKey = properties.getProperty("api.key");
             System.out.println(apiKey);
         } catch (IOException e) {
             e.printStackTrace();
